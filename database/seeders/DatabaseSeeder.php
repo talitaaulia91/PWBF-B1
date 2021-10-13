d5d862ca0ed8437fd941c52f75a3da4dfa18764a
talitaaulia91 <hamimma.talita.aulia-2020@vokasi.unair.ac.id>
2021-10-13 16:25:47 +0700

    benerin route home

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Seed the application's database.
@@ -13,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+ 
     }
 }
